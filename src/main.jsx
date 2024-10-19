67b800b0ee03b4f5b2b969b31373cdac7ea08a40
Kartikay-Garg18 <kartikaygarg777@gmail.com>
2024-10-19 15:41:00 +0530

    Header updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
