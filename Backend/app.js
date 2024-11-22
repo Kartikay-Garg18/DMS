4de7cc5c9eae14d71fb5029dab1e785a4b84233c
Kartikay-Garg18 <kartikaygarg777@gmail.com>
2024-11-22 16:14:02 +0530

    added utils

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import User from './models/user.js';
+import User from './src/models/user.models.js';
+
 
 const app = express();
 const PORT = 5000;
--- Backend/src/models/user.models.js
+++ Backend/src/models/user.models.js
@@ -1,14 +1,19 @@
 import mongoose from 'mongoose';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
 const userSchema = new mongoose.Schema(
   {
     name:{
       type:String,
       required:true,
+      trim: true
     },
     email:{
       type:String,
-      required:true, 
-      unique:true,
+      required: true, 
+      unique: true,
+      trim:  true
     },
     password:{
       type:String,
@@ -20,12 +25,43 @@ const userSchema = new mongoose.Schema(
       required: true,
       minlength:10,
       maxlength : 10
-        },
+    },
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+
+    next();
+
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id : this._id,
+        email : this.email,
+        name : this.name
+    }, process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    })
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id : this._id,
+    }, process.env.REFRESH_TOKEN_SECRET,{
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    })
+}
 
 const User = mongoose.model('User', userSchema);
 export default User;
