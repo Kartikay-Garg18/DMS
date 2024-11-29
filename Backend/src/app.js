321f113d106661608282391832eb2c81e5c576c5
Kartikay Garg <114226144+Kartikay-Garg18@users.noreply.github.com>
2024-11-30 01:58:40 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,10 +4,16 @@ import cors from "cors"
 
 const app = express()
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+const corsOptions = {
+  origin: 'https://daan-dusky.vercel.app', // Allow your frontend origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({
